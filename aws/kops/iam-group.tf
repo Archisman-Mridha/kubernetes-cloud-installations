f61b8560973475a428d56c1c0095b616ecc3655d
Archisman-Mridha <archismanmridha12345@gmail.com>
2023-03-06 23:38:18 +0530

    `kops-with-aws` preliminaries: creating iam groups, users and policies and performing attachments

--- aws/kops/iam-group.tf
+++ aws/kops/iam-group.tf
@@ -1,27 +1,27 @@
-resource "aws_iam_group" "kops_iam_group" {
-    name= "kops_iam_group"
+resource "aws_iam_group" "iam_group" {
+    name= "kops"
 }
 
 //-- policies
 
     resource "aws_iam_group_policy_attachment" "ec2_full_access" {
         policy_arn= "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-        group= aws_iam_group.kops_iam_group.name
+        group= aws_iam_group.iam_group.name
     }
 
     resource "aws_iam_group_policy_attachment" "s3_full_access" {
         policy_arn= "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-        group= aws_iam_group.kops_iam_group.name
+        group= aws_iam_group.iam_group.name
     }
 
     resource "aws_iam_group_policy_attachment" "vpc_full_access" {
         policy_arn= "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-        group= aws_iam_group.kops_iam_group.name
+        group= aws_iam_group.iam_group.name
     }
 
     resource "aws_iam_group_policy_attachment" "iam_full_access" {
         policy_arn= "arn:aws:iam::aws:policy/IAMFullAccess"
-        group= aws_iam_group.kops_iam_group.name
+        group= aws_iam_group.iam_group.name
     }
 
 //--
--- aws/kops/iam-users.tf
+++ aws/kops/iam-users.tf
@@ -1,19 +1,19 @@
-resource "aws_iam_user" "kops" {
+resource "aws_iam_user" "iam_user" {
     name= "kops"
 }
 
 // adding the `kops` iam user to the `aws_kops_Demo` iam group
-resource "aws_iam_group_membership" "kops_kops_iam_group_membership" {
-    name= "kops_kops_iam_group_membership"
+resource "aws_iam_group_membership" "iam_group_membership" {
+    name= "kops_user_kops_group_membership"
 
-    group= aws_iam_group.kops_iam_group.name
-    users= [aws_iam_user.kops.name]
+    group= aws_iam_group.iam_group.name
+    users= [aws_iam_user.iam_user.name]
 }
 
 //-- outputting the access and secret keys of `kops` iam user
 
     resource "aws_iam_access_key" "kops_iam_access_key" {
-        user= aws_iam_user.kops.name
+        user= aws_iam_user.iam_user.name
     }
 
     output "kops_access_key" {
